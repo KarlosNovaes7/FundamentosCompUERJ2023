2c2ed4d50ae78b86f484b1429a805059bdc9206b
KarlosNovaes7 <94071446+KarlosNovaes7@users.noreply.github.com>
2022-09-10 11:24:45 -0300

    update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,13 @@
-print('Esté é o meu primeiro programa em Pythoon utilizando a IDE da Jetbrains, o PyCharm')
+nota1=(float(input('Qual é a sua nota da P1? ')))
+nota2=(float(input('Qual é a sua nota da P2? ')))
+media=((nota1+nota2)/2)
+if media >= 7.0:
+    print("Aluno Aprovado!")
+elif media <= 4.0 and media < 7.0:
+    print("Aluno de PF!")
+else:
+    print('Aluno reprovado!')
+
+
+
+print('Esté é o meu primeiro programa em Python utilizando a IDE da Jetbrains, o PyCharm')
