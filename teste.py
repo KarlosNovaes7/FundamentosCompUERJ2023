bbe118716b91aafdb77dc786ab9db4e47ede337a
KarlosNovaes7 <94071446+KarlosNovaes7@users.noreply.github.com>
2022-09-10 11:36:46 -0300

    .

--- teste.py
+++ teste.py
@@ -3,11 +3,11 @@ nota2=(float(input('Qual é a sua nota da P2? ')))
 media=((nota1+nota2)/2)
 if media >= 7.0:
     print("Aluno Aprovado!")
-elif media <= 4.0 and media < 7.0:
+elif  media >= 4.0 and media< 7.0:
     print("Aluno de PF!")
 else:
     print('Aluno reprovado!')
 
-
+print(type(media))
 
 print('Esté é o meu primeiro programa em Python utilizando a IDE da Jetbrains, o PyCharm')
