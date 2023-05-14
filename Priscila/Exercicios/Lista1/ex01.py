d2a37c88e83cff2bfe2006787353800794112ab6
Karlos Fernandes <94071446+KarlosNovaes7@users.noreply.github.com>
2023-05-14 15:24:27 -0300

    Listas Priscila
    Adicionando ao repo, os exercícios da Lista da Priscilla

---
+++ Priscila/Exercicios/Lista1/ex01.py
@@ -0,0 +1,15 @@
+n1=int(input('Digite o primeiro numero: '))
+troca=0
+print(f'o número digitado foi {n1}\n')
+n2=int(input('Agora digite o segundo número: '))
+print(f'o segundo número digitado foi {n2} \n')
+print('realizando transferência... \n')
+troca=n1
+n1=n2
+n2=troca
+print('... \n')
+print(' Troca realizada ! \n')
+print(f' Agora o primeiro número é {n1} e o segundo é {n2}')
+
+
+      
