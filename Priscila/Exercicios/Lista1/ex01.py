f4afdc6a1e59efea40c3e83520e1151c405ca44a
Karlos Fernandes <94071446+KarlosNovaes7@users.noreply.github.com>
2023-05-14 15:49:18 -0300

    Update ex01.py

--- Priscila/Exercicios/Lista1/ex01.py
+++ Priscila/Exercicios/Lista1/ex01.py
@@ -11,5 +11,15 @@ print('... \n')
 print(' Troca realizada ! \n')
 print(f' Agora o primeiro número é {n1} e o segundo é {n2}')
 
+'''
+pseudocodigo
+1. Leia o valor de A
+2. Leia valor de B
+3. Crie ma variável temporária chamada temp
+4. Atribua  valor de A a temp
+5. Atribua o valor de B a A
+6. Atribua o valor de temp a B
+7. Imprima os valores atualizados de A e B
+'''
 
       
