d0eb24d67e285612a1871acff934abc6bfb6b042
Karlos Fernandes <94071446+KarlosNovaes7@users.noreply.github.com>
2023-05-15 21:05:11 -0300

    Update ex02.py

--- Priscila/Exercicios/Lista1/ex02.py
+++ Priscila/Exercicios/Lista1/ex02.py
@@ -1,13 +1,14 @@
-deslocamento=float(input('Digite o tamanho do trajeto entre dois pontos em Quilometros: \n'))
+deslocamento=float(input(' Digite o tamanho do trajeto entre dois pontos em Quilometros: \n'))
 velocidade=float(input(' Agora digite a sua velocidade por hora no trajeto : \n'))
 tempo=(deslocamento/velocidade)
 print(f' O tempo descrito no trajeto selecionado foi de: {tempo} horas')
 
-'''
-Pseudocódigo
-1-leia a varíavel deslocamento
-2- leia a variável velocidade
-3- crie a variável tempo
-4- tempo= deslocamento/velocidade
-5- imprima variavel tempo
+''' pseudocódigo
+inicio algoritmo
+ float deslocamento, velocidade, tempo:
+ leia deslocamento
+ leia velocidade
+ tempo <= deslocamento/velocidade
+ imprima velocidade
+fim
 '''
